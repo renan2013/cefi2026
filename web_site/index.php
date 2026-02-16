ffa6d5aa0553c97150080b260aeb467757ef1cca
JimenaJuarez <jimenahidalgpjuarez888@gmail.com>
2026-02-16 17:21:02 -0600

    chore: Comentar secciones de servicios y 'sobre nosotros'

--- web_site/index.php
+++ web_site/index.php
@@ -1,8 +1,8 @@
 <?php include 'header.php'; ?>
 
 <?php include 'sections/carousel.php'; ?>
-<?php include 'sections/service.php'; ?>
-<?php include 'sections/about.php'; ?>
+<?php // include 'sections/service.php'; ?>
+<?php // include 'sections/about.php'; ?>
 <?php include 'sections/categories.php'; ?>
 <?php include 'sections/courses.php'; ?>
 <?php include 'sections/team.php'; ?>
