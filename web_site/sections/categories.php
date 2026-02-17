8c65e2d8172abd0849ddff08e99f9c8fa1869bd5
JimenaJuarez <jimenahidalgpjuarez888@gmail.com>
2026-02-17 10:50:34 -0600

    fix: Aplicar max-height a los enlaces de los cards de categorías para unificar altura

--- web_site/sections/categories.php
+++ web_site/sections/categories.php
@@ -6,16 +6,7 @@
                 <h1 class="mb-5">Escuelas de Aprendizaje</h1>
             </div>
             <div class="row g-3">
-                        <div class="col-lg-3 col-md-6 wow zoomIn" data-wow-delay="0.1s">
-                            <a class="position-relative d-block overflow-hidden category-card" href="">
-                                <img class="img-fluid w-100 h-100" src="img/cat-1.jpg" alt="" style="object-fit: cover;">
-                                <div class="category-gradient"></div>
-                                <div class="text-center position-absolute bottom-0 end-0 py-2 px-3" style="margin: 1px; z-index: 2;">
-                                    <h5 class="m-0 text-white">Web Design</h5>
-                                    <small class="text-white">49 Courses</small>
-                                </div>
-                            </a>
-                        </div>
+                        
                         <div class="col-lg-3 col-md-6 wow zoomIn" data-wow-delay="0.3s">
                             <a class="position-relative d-block overflow-hidden category-card" href="">
                                 <img class="img-fluid w-100 h-100" src="img/cat-2.jpg" alt="" style="object-fit: cover;">
@@ -36,16 +27,7 @@
                                 </div>
                             </a>
                         </div>
-                <div class="col-lg-3 col-md-6 wow zoomIn" data-wow-delay="0.7s">
-                    <a class="position-relative d-block overflow-hidden category-card" href="">
-                        <img class="img-fluid w-100 h-100" src="img/cat-4.jpg" alt="" style="object-fit: cover;">
-                        <div class="category-gradient"></div>
-                        <div class="text-center position-absolute bottom-0 end-0 py-2 px-3" style="margin:  1px; z-index: 2;">
-                            <h5 class="m-0 text-white">Online Marketing</h5>
-                            <small class="text-white">49 Courses</small>
-                        </div>
-                    </a>
-                </div>
+                
             </div>
         </div>
     </div>
