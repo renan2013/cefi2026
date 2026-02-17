04012eeea3741eab25ac6e420e837431aff05024
JimenaJuarez <jimenahidalgpjuarez888@gmail.com>
2026-02-17 13:36:16 -0600

    feat: Reducir degradado del carrusel a 20% de la altura

--- web_site/sections/carousel.php
+++ web_site/sections/carousel.php
@@ -3,7 +3,7 @@
         <div class="owl-carousel header-carousel position-relative">
             <div class="owl-carousel-item position-relative">
                 <img class="img-fluid" src="img/carousel-1.jpg" alt="">
-                <div class="position-absolute top-0 start-0 w-100 h-100 d-flex align-items-end" style="background: linear-gradient(to top, rgba(0, 0, 0, 0.8) 0%, rgba(0, 80, 40, 0.5) 20%, transparent 100%);">
+                <div class="position-absolute top-0 start-0 w-100 h-100 d-flex align-items-end" style="background: linear-gradient(to top, rgba(0, 0, 0, 0.8) 0%, rgba(0, 80, 40, 0.5) 10%, transparent 20%);">
                     <div class="container">
                         <div class="row justify-content-start">
                                                         <div class="col-sm-10 col-lg-8" style="margin-top: auto;">
