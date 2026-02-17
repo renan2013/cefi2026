29f7b4ba51a9843848e2e85e5e4d23e2962cc856
JimenaJuarez <jimenahidalgpjuarez888@gmail.com>
2026-02-17 11:03:08 -0600

    elimne el titulo en azul del carrousell

--- web_site/sections/carousel.php
+++ web_site/sections/carousel.php
@@ -7,7 +7,7 @@
                     <div class="container">
                         <div class="row justify-content-start">
                             <div class="col-sm-10 col-lg-8">
-                                <h5 class="text-primary text-uppercase mb-3 animated slideInDown">Best Online Courses</h5>
+                                
                                 <h3 class="display-4 text-white animated slideInDown" style="font-size: 2.5rem;">The Best Online Learning Platform</h3>
                                 <p class="fs-5 text-white mb-4 pb-2">Vero elitr justo clita lorem. Ipsum dolor at sed stet sit diam no. Kasd rebum ipsum et diam justo clita et kasd rebum sea sanctus eirmod elitr.</p>
                                 <a href="" class="btn btn-primary py-md-3 px-md-5 me-3 animated slideInLeft">Ver más</a>
