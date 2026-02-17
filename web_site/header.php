fb27d3efa2df0bdfcbbe89a6e504447632b0abee
JimenaJuarez <jimenahidalgpjuarez888@gmail.com>
2026-02-17 17:16:36 -0600

    fix: Quitar borde blanco de los iconos de redes sociales en el header

--- web_site/header.php
+++ web_site/header.php
@@ -67,16 +67,16 @@
                 <a href="contact.php" class="nav-item nav-link <?php echo ($currentPage == 'contact.php') ? 'active' : ''; ?>">Contacto</a>
             </div>
             <div class="d-flex align-items-center">
-                <a class="btn btn-sm-square btn-outline-light mx-1" href="https://www.facebook.com/CEFI.COSTARICA?mibextid=ZbWKwL" target="_blank" title="Síguenos en Facebook">
+                <a class="btn btn-sm-square mx-1" href="https://www.facebook.com/CEFI.COSTARICA?mibextid=ZbWKwL" target="_blank" title="Síguenos en Facebook">
                     <i class="fab fa-facebook-f"></i>
                 </a>
-                <a class="btn btn-sm-square btn-outline-light mx-1" href="https://www.instagram.com/cefi_cr?igsh=ZnNrZDRhdTB6Z3I0" target="_blank" title="Estamos en Instagram">
+                <a class="btn btn-sm-square mx-1" href="https://www.instagram.com/cefi_cr?igsh=ZnNrZDRhdTB6Z3I0" target="_blank" title="Estamos en Instagram">
                     <i class="fab fa-instagram"></i>
                 </a>
-                <a class="btn btn-sm-square btn-outline-light mx-1" href="https://youtube.com/@ceficr?si=Prh18hZuQhWG6hdY" target="_blank" title="Estamos en Youtube">
+                <a class="btn btn-sm-square mx-1" href="https://youtube.com/@ceficr?si=Prh18hZuQhWG6hdY" target="_blank" title="Estamos en Youtube">
                     <i class="fab fa-youtube"></i>
                 </a>
-                <a class="btn btn-sm-square btn-outline-light mx-1" href="https://www.tiktok.com/@cefi_cr?_t=8jw1rg8zAJh&_r=1" target="_blank" title="Estamos en Tiktok">
+                <a class="btn btn-sm-square mx-1" href="https://www.tiktok.com/@cefi_cr?_t=8jw1rg8zAJh&_r=1" target="_blank" title="Estamos en Tiktok">
                     <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" class="bi bi-tiktok" viewBox="0 0 16 16">
                         <path d="M9 0h1.98c.144.715.54 1.617 1.235 2.512C12.895 3.389 13.797 4 15 4v2c-1.753 0-3.07-.814-4-1.829V11a5 5 0 1 1-5-5v2a3 3 0 1 0 3 3z" />
                     </svg>
