944e8a5c7f7d604f42594bf0f2b6e85a90a95916
JimenaJuarez <jimenahidalgpjuarez888@gmail.com>
2026-02-17 16:40:29 -0600

    feat: Bajar palabra 'Contacto' en la página de contacto

--- web_site/contact.php
+++ web_site/contact.php
@@ -5,7 +5,7 @@
     <div class="container-fluid bg-primary py-5 mb-5 page-header">
         <div class="container py-5">
             <div class="row justify-content-center">
-                <div class="col-lg-10 text-center">
+                <div class="col-lg-10 text-center py-5">
                     <h1 class="display-3 text-white animated slideInDown">Contacto</h1>
                   
                 </div>
