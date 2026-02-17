b205dc9d5ba64ba1eae2e402a12b3f5b6d472276
JimenaJuarez <jimenahidalgpjuarez888@gmail.com>
2026-02-17 11:08:08 -0600

    eliminando iconos de redes sociales de team

--- web_site/sections/team.php
+++ web_site/sections/team.php
@@ -11,13 +11,7 @@
                         <div class="overflow-hidden">
                             <img class="img-fluid" src="img/team-1.jpg" alt="">
                         </div>
-                        <div class="position-relative d-flex justify-content-center" style="margin-top: -23px;">
-                            <div class="bg-light d-flex justify-content-center pt-2 px-1">
-                                <a class="btn btn-sm-square btn-primary mx-1" href=""><i class="fab fa-facebook-f"></i></a>
-                                <a class="btn btn-sm-square btn-primary mx-1" href=""><i class="fab fa-twitter"></i></a>
-                                <a class="btn btn-sm-square btn-primary mx-1" href=""><i class="fab fa-instagram"></i></a>
-                            </div>
-                        </div>
+                        
                         <div class="text-center p-4">
                             <h5 class="mb-0">Instructor Name</h5>
                             <small>Designation</small>
