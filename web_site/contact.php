a3d498780eeffae812d01c9ccf63d94b1b7cbd61
JimenaJuarez <jimenahidalgpjuarez888@gmail.com>
2026-02-17 16:23:49 -0600

    contacto

--- web_site/contact.php
+++ web_site/contact.php
@@ -48,7 +48,7 @@
                 </div>
                 <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.3s">
                     <iframe class="position-relative rounded w-100 h-100"
-                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3001156.4288297426!2d-78.01371936852176!3d42.72876761954724!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4ccc4bf0f123a5a9%3A0xddcfc6c1de189567!2sNew%20York%2C%20USA!5e0!3m2!1sen!2sbd!4v1603794290143!5m2!1sen!2sbd"
+                        src="https://www.google.com/maps/place/Valle+del+Sol/@10.205615,-83.773549,16z/data=!4m6!3m5!1s0x8fa0b808a57100f3:0xe2952c220cefaeb3!8m2!3d10.2056146!4d-83.773549!16s%2Fg%2F11byp6bjkx?hl=en&entry=ttu&g_ep=EgoyMDI2MDIxMS4wIKXMDSoASAFQAw%3D%3D"
                         frameborder="0" style="min-height: 300px; border:0;" allowfullscreen="" aria-hidden="false"
                         tabindex="0"></iframe>
                 </div>
