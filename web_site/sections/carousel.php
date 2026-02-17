160f09b579e72d18d5a07ad3e5f6773db9f19455
JimenaJuarez <jimenahidalgpjuarez888@gmail.com>
2026-02-17 13:25:10 -0600

    feat: Ajustar carrusel (quitar botón, bajar texto, achicar titular)

--- web_site/sections/carousel.php
+++ web_site/sections/carousel.php
@@ -6,13 +6,10 @@
                 <div class="position-absolute top-0 start-0 w-100 h-100 d-flex align-items-end" style="background: linear-gradient(to top, rgba(0, 0, 0, 0.8) 0%, rgba(0, 160, 77, 0.7) 20%, rgba(0, 160, 77, 0) 40%, transparent 100%);">
                     <div class="container">
                         <div class="row justify-content-start">
-                            <div class="col-sm-10 col-lg-8">
-                                
-                                <h3 class="display-4 text-white animated slideInDown" style="font-size: 2.5rem;">The Best Online Learning Platform</h3>
-                                <p class="fs-5 text-white mb-4 pb-2">Vero elitr justo clita lorem. Ipsum dolor at sed stet sit diam no. Kasd rebum ipsum et diam justo clita et kasd rebum sea sanctus eirmod elitr.</p>
-                                <a href="" class="btn py-md-3 px-md-5 me-3 animated slideInLeft" style="background-color: var(--primary) !important; border-color: var(--primary) !important;">Ver más</a>
-                                
-                            </div>
+                                                        <div class="col-sm-10 col-lg-8" style="padding-bottom: 2rem;">
+                                                            <h3 class="display-4 text-white animated slideInDown" style="font-size: 2.2rem;">The Best Online Learning Platform</h3>
+                                                            <p class="fs-5 text-white mb-4 pb-2">Vero elitr justo clita lorem. Ipsum dolor at sed stet sit diam no. Kasd rebum ipsum et diam justo clita et kasd rebum sea sanctus eirmod elitr.</p>
+                                                        </div>
                         </div>
                     </div>
                 </div>
