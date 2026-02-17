bb65558e1c447f5c46dd4e09d1f398e82fc2c40e
JimenaJuarez <jimenahidalgpjuarez888@gmail.com>
2026-02-17 13:32:28 -0600

    revert: Restaurar degradado del carrusel a su estado anterior

--- web_site/sections/carousel.php
+++ web_site/sections/carousel.php
@@ -3,7 +3,7 @@
         <div class="owl-carousel header-carousel position-relative">
             <div class="owl-carousel-item position-relative">
                 <img class="img-fluid" src="img/carousel-1.jpg" alt="">
-                <div class="position-absolute top-0 start-0 w-100 h-100 d-flex align-items-end" style="background: linear-gradient(to top, rgba(0, 0, 0, 0.8) 0%, rgba(0, 160, 77, 0.5) 20%, transparent 100%);">
+                <div class="position-absolute top-0 start-0 w-100 h-100 d-flex align-items-end" style="background: linear-gradient(to top, rgba(0, 0, 0, 0.8) 0%, rgba(0, 160, 77, 0.5) 20%, rgba(0, 160, 77, 0) 40%, transparent 100%);">
                     <div class="container">
                         <div class="row justify-content-start">
                                                         <div class="col-sm-10 col-lg-8" style="margin-top: auto;">
