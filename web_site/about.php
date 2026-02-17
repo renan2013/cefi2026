ba99580d937b78ecb8395dfc6ecec47d0f0008f6
JimenaJuarez <jimenahidalgpjuarez888@gmail.com>
2026-02-17 16:57:11 -0600

    fix: Alinear logo y texto a la parte superior en la sección 'Quiénes Somos'

--- web_site/about.php
+++ web_site/about.php
@@ -21,7 +21,7 @@
     <!-- About Start -->
     <div class="container-xxl py-5">
         <div class="container">
-            <div class="row g-5">
+            <div class="row g-5 align-items-start">
                 <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.1s">
                     <div class="position-relative h-100">
                         <img class="img-fluid position-absolute w-100 h-100" src="img/logo_cefi.svg" alt="Logo CEFI" style="object-fit: contain;">
