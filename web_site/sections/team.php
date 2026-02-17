c43c9efc6e3a4f00acbeb064b80748b38c720777
JimenaJuarez <jimenahidalgpjuarez888@gmail.com>
2026-02-17 11:10:48 -0600

    aditando team

--- web_site/sections/team.php
+++ web_site/sections/team.php
@@ -2,8 +2,8 @@
     <div class="container-xxl py-5">
         <div class="container">
             <div class="text-center wow fadeInUp" data-wow-delay="0.1s">
-                <h6 class="section-title bg-white text-center text-primary px-3">Instructors</h6>
-                <h1 class="mb-5">Expert Instructors</h1>
+                <h6 class="section-title bg-white text-center text-primary px-3">Instructores</h6>
+                <h1 class="mb-5">Expertos Instructores</h1>
             </div>
             <div class="row g-4">
                 <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
