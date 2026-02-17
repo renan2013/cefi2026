f5a362846a2481d61c54b85612a66d3654c06aa3
JimenaJuarez <jimenahidalgpjuarez888@gmail.com>
2026-02-16 18:36:42 -0600

    feat: Cambiar 'Categories' por 'Escuelas' en el título de la sección

--- web_site/sections/categories.php
+++ web_site/sections/categories.php
@@ -2,7 +2,7 @@
     <div class="container-xxl py-5 category">
         <div class="container">
             <div class="text-center wow fadeInUp" data-wow-delay="0.1s">
-                <h6 class="section-title bg-white text-center text-primary px-3">Categories</h6>
+                <h6 class="section-title bg-white text-center text-primary px-3">Escuelas</h6>
                 <h1 class="mb-5">Escuelas de Aprendizaje</h1>
             </div>
             <div class="row g-3">
