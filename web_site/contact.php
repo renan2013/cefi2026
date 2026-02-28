153bff1d82e61d919f7094eed2e38cb6b21b0932
JimenaJuarez <jimenahidalgpjuarez888@gmail.com>
2026-02-28 11:17:36 -0600

    style: Unificar diseño de página de contacto con about.php y añadir logo

--- web_site/contact.php
+++ web_site/contact.php
@@ -1,73 +1,73 @@
 <?php include 'header.php'; ?>
 
-
     <!-- Header Start -->
-    <div class="container-fluid bg-primary py-5 mb-5 page-header">
+    <div class="container-fluid bg-primary py-5 mb-5 page-header" style="background: linear-gradient(rgba(24, 29, 56, .7), rgba(24, 29, 56, .7)), url('img/carousel-1.jpg'); background-size: cover;">
         <div class="container py-5">
             <div class="row justify-content-center">
-                <div class="col-lg-10 text-center py-5">
-                    <h1 class="display-3 text-white animated slideInDown" style="margin-top: 100px;">Contacto</h1>
-                  
+                <div class="col-lg-10 text-center">
+                    <h1 class="display-3 text-white animated slideInDown">Contacto</h1>
                 </div>
             </div>
         </div>
     </div>
     <!-- Header End -->
 
-
     <!-- Contact Start -->
     <div class="container-xxl py-5">
         <div class="container">
-            <div class="text-center wow fadeInUp" data-wow-delay="0.1s">
-                <h6 class="section-title bg-white text-center text-primary px-3">Contacto</h6>
-                <h1 class="mb-5">Contacto CEFI</h1>
-            </div>
-            <div class="row g-4">
-                <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
-                    <h5>Donde estamos</h5>
-                    <p class="mb-4">Guápiles, 50mts norte de la bomba EUSSE, en el Centro Educativo Valle Del Sol.</p>
+            <div class="row g-5 align-items-center mb-5">
+                <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.1s">
+                    <img class="img-fluid" src="img/logo_cefi.svg" alt="Logo CEFI" style="max-height: 400px; margin: auto; display: block;">
+                </div>
+                <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.3s">
+                    <h6 class="section-title bg-white text-start text-primary pe-3">Contacto</h6>
+                    <h1 class="mb-4">Ponte en contacto con nosotros</h1>
+                    <p class="mb-4">Estamos aquí para ayudarte en tu proceso de formación integral. No dudes en escribirnos por cualquiera de nuestros canales oficiales.</p>
                     
                     <div class="d-flex align-items-center mb-3">
-                        <div class="d-flex align-items-center justify-content-center flex-shrink-0 bg-primary" style="width: 50px; height: 50px;">
+                        <div class="d-flex align-items-center justify-content-center flex-shrink-0 bg-primary rounded-circle" style="width: 50px; height: 50px;">
                             <i class="fa fa-phone-alt text-white"></i>
                         </div>
                         <div class="ms-3">
-                            <h5 class="text-primary">Telefonos</h5>
+                            <h5 class="text-primary m-0">Teléfonos</h5>
                             <p class="mb-0">(506) 2711-1010 - WhatsApp: 8992-9180</p>
                         </div>
                     </div>
                     <div class="d-flex align-items-center">
-                        <div class="d-flex align-items-center justify-content-center flex-shrink-0 bg-primary" style="width: 50px; height: 50px;">
+                        <div class="d-flex align-items-center justify-content-center flex-shrink-0 bg-primary rounded-circle" style="width: 50px; height: 50px;">
                             <i class="fa fa-envelope-open text-white"></i>
                         </div>
                         <div class="ms-3">
-                            <h5 class="text-primary">Email</h5>
+                            <h5 class="text-primary m-0">Email</h5>
                             <p class="mb-0">info@ceficr.com</p>
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.3s">
-                    <iframe class="position-relative rounded w-100 h-100"
+            </div>
+
+            <div class="row g-4">
+                <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.1s">
+                    <h5 class="mb-3">Ubicación</h5>
+                    <p class="mb-4"><i class="fa fa-map-marker-alt text-primary me-2"></i>Guápiles, 50mts norte de la bomba EUSSE, en el Centro Educativo Valle Del Sol.</p>
+                    <iframe class="position-relative rounded w-100"
                         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3928.790515829662!2d-83.77335!3d10.20589!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8fa743b00f5c3a2f%3A0x8e8c1b0a8e1e7b1c!2sCentro%20Educativo%20Valle%20Del%20Sol!5e0!3m2!1sen!2scr"
-                        frameborder="0" style="min-height: 300px; border:0;" allowfullscreen="" aria-hidden="false"
+                        frameborder="0" style="min-height: 350px; border:0; width: 100%;" allowfullscreen="" aria-hidden="false"
                         tabindex="0"></iframe>
                 </div>
-                <div class="col-lg-4 col-md-12 wow fadeInUp" data-wow-delay="0.5s">
-                    <form>
-                        <div class="row g-3">
-                            
-                            <div class="col-12">
-                                <a href="https://wa.me/50689929180" class="btn btn-primary w-100 py-3">
-                                    Whatsapp <i class="fab fa-whatsapp ms-2"></i>
-                                </a>
-                            </div>
-                        </div>
-                    </form>
+                <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.5s">
+                    <h5 class="mb-3">Canal de Atención Directa</h5>
+                    <p class="mb-4">Haz clic en el botón de abajo para iniciar una conversación directa con nuestro equipo de admisiones a través de WhatsApp.</p>
+                    <div class="bg-light p-4 rounded shadow-sm text-center">
+                        <i class="fab fa-whatsapp text-success mb-3" style="font-size: 3rem;"></i>
+                        <h4 class="mb-3">WhatsApp CEFI</h4>
+                        <a href="https://wa.me/50689929180" target="_blank" class="btn btn-success btn-lg w-100 py-3">
+                            Hablar con un asesor ahora
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
     <!-- Contact End -->
-
 
 <?php include 'footer.php'; ?>
